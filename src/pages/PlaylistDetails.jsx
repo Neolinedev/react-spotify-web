8dddd6dfca9754a9d57272e5b8b6fb6b70f27fcc
mrsaddam32 <neolinedev@gmail.com>
2022-10-16 03:10:07 +0700

    add user playlist and playlist details

--- src/pages/PlaylistDetails.jsx
+++ src/pages/PlaylistDetails.jsx
@@ -1,5 +1,5 @@
-import React from "react";
-import Playlists from "../constants/playlists/playlist.json";
+import React, { useState, useEffect } from "react";
+import SpotifyWebApi from "spotify-web-api-js";
 import { TabTitle } from "../utils/GlobalFunctions";
 import { useLocation } from "react-router-dom";
 import Grid from "@mui/material/Grid";
@@ -12,114 +12,129 @@ import TableCell from "@mui/material/TableCell";
 import TableContainer from "@mui/material/TableContainer";
 import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
-
 import ArrowBackIosNewOutlinedIcon from "@mui/icons-material/ArrowBackIosNewOutlined";
 
 export const PlaylistDetails = () => {
   let index = 1;
-  const { pathname } = useLocation();
-  const id = pathname.split("/")[2];
-  const data = Playlists;
+  const spotify = new SpotifyWebApi();
+  const location = useLocation();
+  const playlistId = location.pathname.split("/")[2];
+  const [playlist, setPlaylist] = useState(null);
+  const [tracks, setTracks] = useState(null);
   const millisToMinutesAndSeconds = (millis) => {
     const minutes = Math.floor(millis / 60000);
     const seconds = ((millis % 60000) / 1000).toFixed(0);
     return minutes + ":" + (seconds < 10 ? "0" : "") + seconds;
   };
 
-  const playlist = data.find((items) => items.id === id);
-  TabTitle(`DamkunzReactSpotify | ${playlist.name}`);
+  useEffect(() => {
+    const _spotifyToken = localStorage.getItem("spotifyToken");
+    if (_spotifyToken) {
+      spotify.setAccessToken(_spotifyToken);
+    }
 
-  const playlistName = playlist.name;
-  const playlistTracks = require(`../constants/playlists/playlist_tracks/${playlistName}.json`);
+    spotify.getPlaylist(playlistId).then((response) => {
+      setPlaylist(response);
+    });
+
+    spotify.getPlaylistTracks(playlistId).then((response) => {
+      setTracks(response);
+    });
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  TabTitle(`DamkunzReactSpotify | ${playlist?.name}`);
 
   return (
     <React.Fragment>
-      <Grid container sx={{ marginTop: 5, display: "flex", justifyContent: "space-between" }}>
-        <Link
-          variant="button"
-          underline="none"
-          href="/playlists"
-          sx={{
-            borderRadius: 50,
-            height: 32,
-            width: 32,
-            display: "flex",
-            justifyContent: "center",
-            alignItems: "center",
-            color: "white",
-            fontSize: 10,
-            backgroundColor: "rgba(0,0,0,0.7)",
-          }}
-        >
-          <ArrowBackIosNewOutlinedIcon />
-        </Link>
-      </Grid>
-      <Grid spacing={2} container sx={{ marginTop: 8 }}>
-        <Grid
-          item
-          xs={12}
-          sm={12}
-          md={2}
-          sx={{
-            marginLeft: { sm: 6, lg: 10 },
-            marginRight: { sm: 13, lg: 10 },
-            display: { xs: "flex", sm: "flex" },
-            justifyContent: { xs: "center", sm: "center", lg: "flex-start" },
-          }}
-        >
-          <Avatar alt={playlist.name} src={playlist.images[0].url} sx={{ width: 250, height: 250, boxShadow: "0 2px 18px 1px rgba(0,0,0,0.4)" }} variant="square" />
-        </Grid>
-        <Grid item xs={12} sm={12} md={8} sx={{ display: "flex", flexWrap: "wrap", flexFlow: "column", justifyContent: "flex-end", marginLeft: 0, marginTop: { xs: 4 } }}>
-          <Typography variant="h2" component="div" sx={{ fontSize: { xs: 13, lg: 12 }, textTransform: "uppercase" }}>
-            Playlist
-          </Typography>
-          <Typography variant="h1" component="div" sx={{ fontSize: { xs: 54, lg: 60 }, fontWeight: 800 }}>
-            {playlist.name}
-          </Typography>
-          <Typography variant="p" component="div" sx={{ fontSize: 16, color: "#B3B3B3" }}>
-            {playlist.description}
-          </Typography>
-          <Typography variant="p" component="div" sx={{ fontSize: 16, fontWeight: 600 }}>
-            {playlist.owner.display_name} • {playlist.tracks.total} songs
-          </Typography>
-        </Grid>
-        <Grid item xs={12} sm={12} md={12} justify="center">
-          <TableContainer sx={{ marginTop: 10 }}>
-            <Table sx={{ minWidth: 650 }} stickyHeader aria-label="simple table">
-              <TableHead>
-                <TableRow>
-                  <TableCell>#</TableCell>
-                  <TableCell align="left">Title</TableCell>
-                  <TableCell align="left">Album</TableCell>
-                  <TableCell align="left">Duration</TableCell>
-                </TableRow>
-              </TableHead>
-              <TableBody>
-                {playlistTracks.tracks.map((track, i) => (
-                  <TableRow key={i} sx={{ "&:last-child td, &:last-child th": { border: 0 }, alignItems: "stretch" }}>
-                    <TableCell component="th" scope="row" sx={{ borderBottom: "none" }}>
-                      {index++}
-                    </TableCell>
-                    <TableCell sx={{ display: "flex", alignItems: "center", borderBottom: "none" }}>
-                      <Avatar alt={track.name} src={track.album.images[0].url} variant="square" />
-                      <Typography variant="p" component="div" sx={{ fontSize: 16, paddingLeft: 2 }}>
-                        {track.name}
-                        {track.album.artists.map((item, i) => (
-                          <Typography variant="p" component="div" sx={{ fontSize: 14, color: "#A7A7A7" }} key={i}>
-                            <span sx={{ display: "inline-block" }}>{item.name}</span>
+      {playlist && (
+        <Grid container sx={{ marginTop: 5, display: "flex", justifyContent: "space-between" }}>
+          <Link
+            variant="button"
+            underline="none"
+            href="/playlists"
+            sx={{
+              borderRadius: 50,
+              height: 32,
+              width: 32,
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+              color: "white",
+              fontSize: 10,
+              backgroundColor: "rgba(0,0,0,0.7)",
+            }}
+          >
+            <ArrowBackIosNewOutlinedIcon />
+          </Link>
+          <Grid spacing={2} container sx={{ marginTop: 8 }}>
+            <Grid
+              item
+              xs={12}
+              sm={12}
+              md={2}
+              sx={{
+                marginLeft: { sm: 6, lg: 10 },
+                marginRight: { sm: 13, lg: 10 },
+                display: { xs: "flex", sm: "flex" },
+                justifyContent: { xs: "center", sm: "center", lg: "flex-start" },
+              }}
+            >
+              <Avatar alt={playlist.name} src={playlist.images[0].url} sx={{ width: 250, height: 250, boxShadow: "0 2px 18px 1px rgba(0,0,0,0.4)" }} variant="square" />
+            </Grid>
+            <Grid item xs={12} sm={12} md={8} sx={{ display: "flex", flexWrap: "wrap", flexFlow: "column", justifyContent: "flex-end", marginLeft: 0, marginTop: { xs: 4 } }}>
+              <Typography variant="h2" component="div" sx={{ fontSize: { xs: 13, lg: 12 }, textTransform: "uppercase" }}>
+                Playlist
+              </Typography>
+              <Typography variant="h1" component="div" sx={{ fontSize: { xs: 54, lg: 60 }, fontWeight: 800 }}>
+                {playlist.name}
+              </Typography>
+              <Typography variant="p" component="div" sx={{ fontSize: 16, color: "#B3B3B3" }}>
+                {playlist.description}
+              </Typography>
+              <Typography variant="p" component="div" sx={{ fontSize: 16, fontWeight: 600 }}>
+                {playlist.owner.display_name} • {playlist.tracks.total} songs
+              </Typography>
+            </Grid>
+            <Grid item xs={12} sm={12} md={12} justify="center">
+              <TableContainer sx={{ marginTop: 10 }}>
+                <Table sx={{ minWidth: 650 }} stickyHeader aria-label="simple table">
+                  <TableHead>
+                    <TableRow>
+                      <TableCell>#</TableCell>
+                      <TableCell align="left">Title</TableCell>
+                      <TableCell align="left">Album</TableCell>
+                      <TableCell align="left">Duration</TableCell>
+                    </TableRow>
+                  </TableHead>
+                  <TableBody>
+                    {tracks?.items.map((track, i) => (
+                      <TableRow key={i} sx={{ "&:last-child td, &:last-child th": { border: 0 }, alignItems: "stretch" }}>
+                        <TableCell component="th" scope="row" sx={{ borderBottom: "none" }}>
+                          {index++}
+                        </TableCell>
+                        <TableCell sx={{ display: "flex", alignItems: "center", borderBottom: "none" }}>
+                          <Avatar alt={track.name} src={track.track.album.images[0].url} variant="square" />
+                          <Typography variant="p" component="div" sx={{ fontSize: 16, paddingLeft: 2 }}>
+                            {track.track.name}
+                            {track.track.artists.map((artist, i) => (
+                              <Typography variant="p" component="div" sx={{ fontSize: 14, color: "#A7A7A7" }} key={i}>
+                                <span sx={{ display: "inline-block" }}>{artist.name}</span>
+                              </Typography>
+                            ))}
                           </Typography>
-                        ))}
-                      </Typography>
-                    </TableCell>
-                    <TableCell sx={{ color: "#B3B3B3", borderBottom: "none" }}>{track.album.name}</TableCell>
-                    <TableCell sx={{ color: "#B3B3B3", borderBottom: "none" }}>{millisToMinutesAndSeconds(track.duration_ms)}</TableCell>
-                  </TableRow>
-                ))}
-              </TableBody>
-            </Table>
-          </TableContainer>
+                        </TableCell>
+                        <TableCell sx={{ color: "#B3B3B3", borderBottom: "none" }}>{track.track.album.name}</TableCell>
+                        <TableCell sx={{ color: "#B3B3B3", borderBottom: "none" }}>{millisToMinutesAndSeconds(track.track.duration_ms)}</TableCell>
+                      </TableRow>
+                    ))}
+                  </TableBody>
+                </Table>
+              </TableContainer>
+            </Grid>
+          </Grid>
         </Grid>
-      </Grid>
+      )}
     </React.Fragment>
   );
 };
--- src/pages/Playlists.jsx
+++ src/pages/Playlists.jsx
@@ -1,6 +1,6 @@
-import * as React from "react";
+import React, { useState, useEffect } from "react";
+import SpotifyWebApi from "spotify-web-api-js";
 import { Link as RouterLink } from "react-router-dom";
-import Playlist from "../constants/playlists/playlist.json";
 import CardActionArea from "@mui/material/CardActionArea";
 import CardContent from "@mui/material/CardContent";
 import CardMedia from "@mui/material/CardMedia";
@@ -10,30 +10,53 @@ import Card from "@mui/material/Card";
 import { TabTitle } from "../utils/GlobalFunctions";
 
 export const Playlists = () => {
-  const data = Playlist;
+  const spotify = new SpotifyWebApi();
+  const [playlists, setPlaylists] = useState(null);
+
+  useEffect(() => {
+    const _spotifyToken = localStorage.getItem("spotifyToken");
+    if (_spotifyToken) {
+      spotify.setAccessToken(_spotifyToken);
+    }
+
+    spotify.getUserPlaylists().then((response) => {
+      setPlaylists(response.items);
+    });
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
   TabTitle("DamkunzReactSpotify | My Playlists");
 
   return (
     <React.Fragment>
       <h1 sx={{ textAlign: "center" }}>My Playlists</h1>
-      <Grid container spacing={{ xs: 1, sm: 2, md: 3 }}>
-        {data.map((items, i) => (
-          <Grid item xs={12} sm={6} md={3} sx={{ display: "flex", justifyContent: "center" }} key={i}>
-            <Card sx={{ maxWidth: { lg: 300, sm: 500, xs: 420 }, borderRadius: 3 }}>
-              <CardActionArea component={RouterLink} to={`/playlist/${items.id}`}>
-                <CardMedia component="img" height="100" image={items.images[0].url} sx={{ height: "fit-content", boxShadow: "0 5px 10px 2px rgba(0,0,0,0.5)" }} />
-                <CardContent>
-                  <Typography gutterBottom variant="h5" component="div" sx={{ fontWeight: 700 }}>
-                    {items.name}
-                  </Typography>
-                  <Typography variant="body2" color="text.secondary">
-                    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Provident omnis ipsam voluptatem magni quibusdam a.
-                  </Typography>
-                </CardContent>
-              </CardActionArea>
-            </Card>
-          </Grid>
-        ))}
+      <Grid container spacing={3}>
+        {playlists ? (
+          playlists.map((playlist, i) => (
+            <Grid item xs={12} sm={6} md={3} sx={{ display: "flex", justifyContent: "center" }} key={i}>
+              <Card sx={{ maxWidth: { lg: 300, sm: 500, xs: 420 }, borderRadius: 3, height: "100%" }}>
+                <CardActionArea
+                  component={RouterLink}
+                  to={`/playlist/${playlist.id}`}
+                  sx={{
+                    position: "relative",
+                    "&:hover": {
+                      transform: "scale(1.05)",
+                      transition: "all 0.3s ease-in-out",
+                    },
+                  }}
+                >
+                  <CardMedia component="img" height="100" image={playlist.images[0].url} sx={{ height: "fit-content", boxShadow: "0 5px 10px 2px rgba(0,0,0,0.5)" }} />
+                </CardActionArea>
+              </Card>
+            </Grid>
+          ))
+        ) : (
+          <Typography variant="h5" sx={{ textAlign: "center", marginTop: 6 }}>
+            Loading...
+          </Typography>
+        )}
       </Grid>
     </React.Fragment>
   );
